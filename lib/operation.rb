ba3fe143318cb473374099537588b09170459376
Marta Bianchini <marta.bianchini24@gmail.com>
2023-02-21 14:59:33 +0000

    implemented Operation class, not tested as no functionality

---
+++ lib/operation.rb
@@ -0,0 +1,19 @@
+class Operation
+  def initialize(date, type, amount)
+    @date = date
+    @type = type
+    @amount = amount
+  end
+
+  def date
+    return @date
+  end
+
+  def type
+    return @type
+  end
+
+  def amount
+    return @amount
+  end
+end

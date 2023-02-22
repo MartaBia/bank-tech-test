76f450a71944586829c3cb22b94ed5ef4c014250
Marta Bianchini <marta.bianchini24@gmail.com>
2023-02-22 10:41:32 +0000

    added test for transaction done in different dates

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -68,4 +68,20 @@ RSpec.describe BankAccount do
       "DATE || CREDIT || DEBIT || BALANCE\n10/01/2023 || || 500.00 || 1000.00\n10/01/2023 || 1500.00 || || 1500.00"
     )
   end
+
+  it 'returns the statement with 2 transaction done in 2 different dates' do
+    bank_account = BankAccount.new([])
+    fixed_date1 = DateTime.new(2023, 01, 10)
+    allow(DateTime).to receive(:now).and_return(fixed_date1)
+
+    bank_account.deposit(1500)
+
+    fixed_date2 = DateTime.new(2023, 01, 13)
+    allow(DateTime).to receive(:now).and_return(fixed_date2)
+    bank_account.withdraw(500)
+    
+    expect(bank_account.get_statement).to eq (
+      "DATE || CREDIT || DEBIT || BALANCE\n13/01/2023 || || 500.00 || 1000.00\n10/01/2023 || 1500.00 || || 1500.00"
+    )
+  end
 end
